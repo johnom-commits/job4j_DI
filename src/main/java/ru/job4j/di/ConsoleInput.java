fb6ede9524c5643e8517f67a8cc7d525a4fc52ad
johnom-commits <johnom2012@yandex.ru>
2020-12-23 13:04:57 +0300

    Scan project

--- src/main/java/ru/job4j/di/ConsoleInput.java
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -1,10 +1,12 @@
 package ru.job4j.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConsoleInput {
 
     public String askStr(String question) {
         System.out.println(question);
         return question;
     }
-
 }
